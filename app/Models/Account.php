ffa1f9c66f39dc5b282fdff78e64c6f58f1c11fa
Rogério Nascimento Santos <rogerionascimentosantos@gmail.com>
2024-02-26 02:27:34 -0300

    ping crm

---
+++ app/Models/Account.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Account extends Model
+{
+    public function users()
+    {
+        return $this->hasMany(User::class);
+    }
+
+    public function organizations()
+    {
+        return $this->hasMany(Organization::class);
+    }
+
+    public function contacts()
+    {
+        return $this->hasMany(Contact::class);
+    }
+
+    public function classroms()
+    {
+        return $this->hasMany(Classroms::class);
+    }
+}
